70fc95a65dbb70bad56b92c3b4509e1a1d273d7e
leka61 <belenka61@gmail.com>
2022-01-16 00:55:07 +0300

    1

---
+++ src/components/topline/topline.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="c-topline">
+        <div class="x-container">
+            
+        </div>
+    </div>
+</template>
+<style lang="scss" src="./topline.scss" scoped>
+
+</style>

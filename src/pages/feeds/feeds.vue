3b37762c561db8a4adced80633c69362f9deaae9
leka61 <belenka61@gmail.com>
2022-01-16 03:28:56 +0300

    1

--- src/pages/feeds/feeds.vue
+++ src/pages/feeds/feeds.vue
@@ -1,3 +1,45 @@
 <template>
-    <h1>Hello world</h1>
+    <div class="topline">
+        <topline>
+            <template #headline>
+                <div class="icon">
+                    <icon name="home"/>
+                </div>
+            </template>
+            <template #content>
+                <ul class="stories">
+                    <li v-for="story in stories" :key="story.id" class="stories-item">
+                        <story-user-item :avatar="story.avatar" :username="story.username" @onPress="handlePress(story.id)"/>
+                    </li>
+                </ul>
+            </template>
+        </topline>
+    </div>
 </template>
+<script>
+import { topline } from '../../components/topline'
+import { icon } from '../../icons'
+import { storyUserItem } from '../../components/storyUserItem'
+import stories from './data.json'
+
+export default {
+  name: 'feeds',
+  components: {
+    topline,
+    icon,
+    storyUserItem
+  },
+  data () {
+    return {
+      stories
+    }
+  }
+}
+</script>
+<style lang="scss" scoped src="./feeds.scss"></style>
+<style lang="scss" scoped>
+    .icon{
+        color:red;
+        width:50px;
+    }
+</style>

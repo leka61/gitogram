db7395ed98fefdd2241109cb6e4858f125263dac
leka61 <belenka61@gmail.com>
2022-02-05 21:13:38 +0300

    4

--- src/router/index.js
+++ src/router/index.js
@@ -5,4 +5,23 @@ const router = createRouter({
   history: createWebHashHistory()
 })
 
+router.beforeEach(async(to, from, next) => {
+  const authRoute = to.name === "auth";
+  if (authRoute) {
+    next();
+    return;
+  }
+  try {
+    const response = await fetch("https://api.github.com/user", {
+      headers: {
+        Authorization: `token ${localStorage.getItem("token")}`
+      }
+    })
+    if (response.status === 401) throw new Error();
+
+    next();
+  } catch (err) {
+    next({ name: "auth" });
+  }
+})
 export default router
